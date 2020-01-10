336563b76e97a11ed63836603ae8339b91562d4f
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-01-10 13:32:56 +0100

    assign icon ids to variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const rock = document.querySelector('rock');
+const paper = document.querySelector('paper');
+const scissors = document.querySelector('scissors');
+const lizard = document.querySelector('lizard');
+const spock = document.querySelector('spock');
