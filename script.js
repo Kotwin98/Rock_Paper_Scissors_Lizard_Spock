4bee3575dad3644d0427736fc760c0ad5967de33
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-01-10 16:43:23 +0100

    update playerMove function

--- script.js
+++ script.js
@@ -12,15 +12,59 @@ console.log(pcMove);
 
 function playerMove(playerMove){
     if(playerMove == "rock" && pcMove == 1){
-        output.innerHTML = 'YOU TIED';
+        output.innerHTML = 'YOU TIED THIS ROUND';
     } else if(playerMove == "rock" && pcMove == 2){
-        output.innerHTML = 'YOU TIED';
+        output.innerHTML = 'YOU LOST THIS ROUND';
     } else if(playerMove == "rock" && pcMove == 3){
-        output.innerHTML = 'YOU TIED';
+        output.innerHTML = 'YOU WON THIS ROUND';
     } else if(playerMove == "rock" && pcMove == 4){
-        output.innerHTML = 'YOU TIED';
+        output.innerHTML = 'YOU WON THIS ROUND';
     } else if(playerMove == "rock" && pcMove == 5){
-        output.innerHTML = 'YOU TIED';
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } 
+    else if(playerMove == "paper" && pcMove == 1){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } else if(playerMove == "paper" && pcMove == 2){
+        output.innerHTML = 'YOU TIED THIS ROUND';
+    } else if(playerMove == "paper" && pcMove == 3){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } else if(playerMove == "paper" && pcMove == 4){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } else if(playerMove == "paper" && pcMove == 5){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } 
+    else if(playerMove == "scissors" && pcMove == 1){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } else if(playerMove == "scissors" && pcMove == 2){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } else if(playerMove == "scissors" && pcMove == 3){
+        output.innerHTML = 'YOU TIED THIS ROUND';
+    } else if(playerMove == "scissors" && pcMove == 4){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } else if(playerMove == "scissors" && pcMove == 5){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } 
+    else if(playerMove == "lizard" && pcMove == 1){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } else if(playerMove == "lizard" && pcMove == 2){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } else if(playerMove == "lizard" && pcMove == 3){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } else if(playerMove == "lizard" && pcMove == 4){
+        output.innerHTML = 'YOU TIED THIS ROUND';
+    } else if(playerMove == "lizard" && pcMove == 5){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } 
+    else if(playerMove == "spock" && pcMove == 1){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } else if(playerMove == "spock" && pcMove == 2){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } else if(playerMove == "spock" && pcMove == 3){
+        output.innerHTML = 'YOU WON THIS ROUND';
+    } else if(playerMove == "spock" && pcMove == 4){
+        output.innerHTML = 'YOU LOST THIS ROUND';
+    } else if(playerMove == "spock" && pcMove == 5){
+        output.innerHTML = 'YOU TIED THIS ROUND';
     }
 }
 
